84dc55250ed9a3b80b633f83cb104581ee912d4e
Matheus Dutra <matheusdutra0207@gmail.com>
2022-09-01 11:45:00 -0300

    updating the exemple, changing is_message_type to Pose

--- exemples/ROSComunication/publishToROS.py
+++ exemples/ROSComunication/publishToROS.py
@@ -11,7 +11,7 @@ from tf.transformations import quaternion_from_euler
 
 # IS
 from is_wire.core import Channel, Subscription
-from is_msgs.camera_pb2 import FrameTransformation
+from is_msgs.common_pb2 import Pose
 
 #  my own
 from streamChannel import StreamChannel
@@ -20,21 +20,18 @@ from streamChannel import StreamChannel
 def getPoseReconstruction():
     message = channel_recontruction.consume_last()
     if type(message) != bool:
-        f = message.unpack(FrameTransformation)
-        tf = f.tf.doubles
-        x_recontruction = tf[0]
-        y_recontruction = tf[1]
-        roll_rad_recontruction = tf[3]
-
-        return np.array([x_recontruction, y_recontruction, roll_rad_recontruction])
+        pose = message.unpack(Pose)
+        x_recontruction = pose.position.x
+        y_recontruction = pose.position.y
+        yaw_rad_recontruction = pose.orientation.yaw
+        return np.array([x_recontruction, y_recontruction, yaw_rad_recontruction])
     else:
         return message
-
 
 def publishToRos():
     rospy.init_node('is')
     pub_pose = rospy.Publisher("~vo", PoseWithCovarianceStamped, queue_size=100)
-    rate = rospy.Rate(10) # 10hz
+    rate = rospy.Rate(rate_publish) # 10hz
     while not rospy.is_shutdown():
         try: 
             position_reconstruction = getPoseReconstruction()
@@ -46,16 +43,12 @@ def publishToRos():
             pose.pose.pose.position.y = position_reconstruction[1]
             pose.pose.pose.orientation.z = quaternion[2]
             pose.pose.pose.orientation.w = quaternion[3]
-
-
-            covarianceIs = 0.14
-
-            pose.pose.covariance = np.array([covarianceIs,   0,   0,   0,   0,   0,
-                                    0,   covarianceIs,   0,   0,   0,   0,
-                                    0,   0,   covarianceIs,   0,   0,   0,
-                                    0,   0,   0,   covarianceIs,   0,   0,
-                                    0,   0,   0,   0,   covarianceIs,   0,
-                                    0,   0,   0,   0,   0,   covarianceIs])
+            pose.pose.covariance = np.array([measurement_covariance_noise,   0,   0,   0,   0,   0,
+                                            0,   measurement_covariance_noise,   0,   0,   0,   0,
+                                            0,   0,   measurement_covariance_noise,   0,   0,   0,
+                                            0,   0,   0,   measurement_covariance_noise,   0,   0,
+                                            0,   0,   0,   0,   measurement_covariance_noise,   0,
+                                            0,   0,   0,   0,   0,   measurement_covariance_noise])
             rospy.loginfo(pose)
             pub_pose.publish(pose)
             rate.sleep()
@@ -63,11 +56,15 @@ def publishToRos():
             pass
 
 if __name__ == '__main__':
-    channel_recontruction = StreamChannel("amqp://10.10.3.188:30000")
-    subscription = Subscription(channel_recontruction)
 
-    aruco_id = 5
-    subscription.subscribe(topic=f"localization.{aruco_id}.aruco")
+    broker_uri= rospy.get_param("is/broker_uri", "amqp://10.10.3.188:30000")
+    aruco_id  =  rospy.get_param("is/aruco_id", 5)
+    measurement_covariance_noise = rospy.get_param("is/measurement_covariance_noise", 0.14)
+    rate_publish = rospy.get_param("is/rate_publish", 10)
+
+    channel_recontruction = StreamChannel(broker_uri)
+    subscription = Subscription(channel_recontruction)
+    subscription.subscribe(topic=f"reconstruction.{aruco_id}.ArUco")
 
     try:
         publishToRos()
